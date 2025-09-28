a340e2f5f9f671bc523f2fcddac9289b279e401f
StupidityNoob <xanansithx0@gmail.com>
2025-09-28 13:43:42 +0700

    Delete Game-list.lua

--- Game-list.lua
+++
@@ -1,5 +0,0 @@
-local Games= {
-  [111510466699934] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Lobby
-  [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory" -- Game
-}
-return Games
