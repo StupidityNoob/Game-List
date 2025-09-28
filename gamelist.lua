ed7eab2e9269aceebe9e978e2a251cbfcc8430c1
StupidityNoob <xanansithx0@gmail.com>
2025-09-28 17:48:30 +0700

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,7 +1,7 @@
 local Games= {
-  [111510466699934] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Lobby
-  [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Game
-  [117233475330049] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Mafia.lua",
-  [17218259625] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/WhosTheSpy.lua"
+  [111510466699934] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Lobby
+  [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Game
+  [117233475330049] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Mafia.lua", -- Mafia
+  [17218259625] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/WhosTheSpy.lua" -- Who's The Spy
 }
 return Games
