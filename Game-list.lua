d38db152db86385cbf6e2816f1f1c033fb561c59
StupidityNoob <xanansithx0@gmail.com>
2025-09-28 13:27:17 +0700

    Update Game-list.lua

--- Game-list.lua
+++ Game-list.lua
@@ -1,4 +1,5 @@
 local Games= {
   [111510466699934] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory"
+  [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory"
 }
 return Games
