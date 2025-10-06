6d590b3098e7b4329464b54d022cef0b2b5184f0
StupidityNoob <xanansithx0@gmail.com>
2025-10-06 19:15:58 +0700

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -3,6 +3,8 @@ local Games= {
   [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Game
   [117233475330049] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Mafia.lua", -- Mafia
   [17218259625] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/WhosTheSpy.lua", -- Who's The Spy
-  [118384840573608] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/BeAbodybuilder" -- Be a Bodybuilder
+  [118384840573608] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/BeAbodybuilder", -- Be a Bodybuilder
+  [132620530679256] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/main/Pushtogether.lua", -- Lobby
+  [73367387755542] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/main/Pushtogether.lua", -- main game
 }
 return Games
